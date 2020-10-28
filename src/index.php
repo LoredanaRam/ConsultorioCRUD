ec66154493383afc56df1a9dc997829b2bca7243
Jose <jmgarcia81@uoc.edu>
2020-10-28 12:28:14 +0100

    diseño de header y navbar finalizado

--- src/index.php
+++ src/index.php
@@ -1,6 +1,9 @@
+<?php require "views/partials/head.php";?>
+
 <?php require "views/partials/header.php";?>
 
-<span>Hola mundo</span>
+<?php require "views/partials/navbar.php";?>
 
+<?php require "views/partials/scripts.php";?>
 
 <?php require "views/partials/footer.php";?>
---
+++ src/views/partials/navbar.php
@@ -0,0 +1,10 @@
+<section>
+    <ul class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link" href="index.php"><i class="fas fa-home"></i></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Create Date</a>
+    </li>
+</ul>
+</section>
