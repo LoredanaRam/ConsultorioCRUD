53db250aad317f34414a116e79c14ed6d263f2d2
alejandra <aleejandrast@gmail.com>
2020-10-28 11:57:28 +0100

    Create form.php

---
+++ src/views/partials/form.php
@@ -0,0 +1,37 @@
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputName4">Name</label>
+      <input type="text" class="form-control" id="inputName4" placeholder="Name">
+    </div>
+
+
+  <div class="form-group col-md-6">
+      <label for="inputSurname4">Password</label>
+      <input type="text" class="form-control" id="inputSurname4" placeholder="Surname">
+    </div>
+    
+  
+    
+  <div class="form-group col-md-4">
+      <label for="inputReason">Reason of the Visit</label>
+      <select id="inputReason" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+        <option>...</option>
+        <option>...</option>
+        <option>...</option>
+
+      </select>
+    </div>
+
+
+
+  <div class="form-group">
+    <label for="inputDescription">Description</label>
+    <input type="text" class="form-control" id="inputDescription" placeholder="Description">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Send</button>
+  <button type="reset" class="btn btn-primary">Reset</button>
+</form>
