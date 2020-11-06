d80d8525bdc84c48915b28dbf0f2cad81159a4e1
pilpod <67429595+pilpod@users.noreply.github.com>
2020-11-06 11:00:27 +0100

    Añadido datos php al formToEdit.php

--- src/views/partials/formToEdit.php
+++ src/views/partials/formToEdit.php
@@ -12,14 +12,14 @@
 
       <div class="modal-body bg-dark">
 
-          <form class="form-group" >
+          <form class="form-group" action="upload" method="POST">
               
               <label for="inputEditName">Nombre</label>
-              <input type="text" class="form-control bg-dark-lighter border-primary" id="inputEditName" placeholder="Name" required>
+              <input type="text" class="form-control bg-dark-lighter border-primary" id="inputEditName" placeholder="Name" value="<?= $appointment->getName() ?>" required>
 
               <label for="inputEditReason">Motivo de la visita</label>
               <select id="inputEditReason" class="form-control bg-dark-lighter  border-primary" required>
-                <option selected placeholder= "Choose an option"> </option>
+                <option selected><?= $appointment->getTopic() ?></option>
                   <option>no me anda el php</option>
                   <option>el perro se comió mi tarea</option>
                   <option>ayudenme porfavor señor Sergi</option>
@@ -28,7 +28,7 @@
 
 
               <label for="inputEditDescription">Detalle su inquietud</label>
-              <input type="text" class="form-control bg-dark-lighter  border-primary" id="inputEditDescription" placeholder="Description" required>
+              <input type="text" class="form-control bg-dark-lighter  border-primary" id="inputEditDescription" placeholder="Description" value="<?= $appointment->getDescription() ?>" required>
                 
             
               <div class="modal-footer border-0">
